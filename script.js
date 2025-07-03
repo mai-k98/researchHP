b82c4c6833d15c6e06f44500eebb2ac55bf10e57
mai-k98 <maikatagiri.work@gmail.com>
2025-07-03 13:41:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+$(function () {
+
+  $(".hamburger-btn").on("click", function () {
+    if ($("header").hasClass("open")) {
+      $("header").removeClass("open");
+    } else {
+      $("header").addClass("open");
+    }
+  });
+  $("nav li").on("click", function () {
+
+    $("header").removeClass("open");
+
+  });
+  $(".mask").on("click", function () {
+
+    $("header").removeClass("open");
+
+  });
+
+
+  let fadeIn = function () {
+  let scroll = $(window).scrollTop();
+  let windowHeight = $(window).height();
+
+  $(".fadein").each(function () {
+    let target = $(this).offset().top;
+    if (scroll > target - windowHeight + 200) {
+      $(this).css({
+        opacity: 1,
+        transform: "translateY(0)"
+      });
+    }
+  });
+};
+$(window).on("scroll", fadeIn);
+fadeIn(); // 初回呼び出しも忘れずに
+
+
+
+  $(".slide-items").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: "30%",
+
+    responsive: [{
+      breakpoint: 820,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "10%",
+      }
+    }]
+  });
+});
+
+//マウスストーカー
+const stalker = document.getElementById('mouse-stalker');
+let hovFlag = false;
+
+document.addEventListener('mousemove', function (e) {
+    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
+
+const linkElem = document.querySelectorAll('a:not(.no_stick_)');
+for (let i = 0; i < linkElem.length; i++) {
+    linkElem[i].addEventListener('mouseover', function (e) {
+        hovFlag = true;
+        stalker.classList.add('is_active');
+
+    });
+    linkElem[i].addEventListener('mouseout', function (e) {
+        hovFlag = false;
+        stalker.classList.remove('is_active');
+    });
+}
